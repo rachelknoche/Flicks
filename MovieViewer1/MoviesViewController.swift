328557f1890ab42b1d5a2a26c461c37758d83c89
Rachel Knoche <Rachel@rachels-mbp.mobile.asu.edu>
2016-01-24 22:01:23 -0700

    Combine

--- MovieViewer1/MoviesViewController.swift
+++ MovieViewer1/MoviesViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AFNetworking
 
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
    
@@ -71,9 +72,25 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         let movie = movies![indexPath.row]
         let title = movie["title"] as! String
         let overview = movie["overview"] as! String
+        let posterPath = movie["poster_path"] as! String
         
+        let baseUrl = "http://image.tmdb.org/t/p/w500/"
+        
+        let imageUrl = NSURL(string: baseUrl + posterPath)
+        
+ 
+        
+        
+        
+        cell.posterView.setImageWithURL(imageUrl!)
         cell.titleLabel.text = title
         cell.overviewLabel.text = overview
+        
+        
+        
+        
+        
+        
 
         print("row\(indexPath.row)")
         return cell
---
+++ Pods/Target Support Files/Pods-MovieViewer1/Pods-MovieViewer1-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MovieViewer1 : NSObject
+@end
+@implementation PodsDummy_Pods_MovieViewer1
+@end
